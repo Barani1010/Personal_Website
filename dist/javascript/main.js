3be47bdfeebcedd2b1f5458dc303116cc984e1d7
Barani10101 <yomnmbarani@gmail.com>
2018-08-23 22:31:49 +0530

    Menu button event and transitions

---
+++ dist/javascript/main.js
@@ -0,0 +1,34 @@
+// Select DOM Items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuPic = document.querySelector(".menu-pic");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set initial state of menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuPic.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    //Set menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuPic.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    //Set menu state
+    showMenu = flase;
+  }
+}
